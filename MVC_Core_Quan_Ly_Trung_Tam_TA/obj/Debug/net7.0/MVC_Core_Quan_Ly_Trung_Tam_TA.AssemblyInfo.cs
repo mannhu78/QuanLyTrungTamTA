3f31a38142f2cec97378eb0132c54ba000679c66
mannhu78 <mannhu593@gmail.com>
2024-01-15 16:57:45 +0700

    menu_moi

--- MVC_Core_Quan_Ly_Trung_Tam_TA/obj/Debug/net7.0/MVC_Core_Quan_Ly_Trung_Tam_TA.AssemblyInfo.cs
+++ MVC_Core_Quan_Ly_Trung_Tam_TA/obj/Debug/net7.0/MVC_Core_Quan_Ly_Trung_Tam_TA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC_Core_Quan_Ly_Trung_Tam_TA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4291066b89340d645c79c5c5815a445314a741a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cba3a9d21228c5821380f9542c9cc4da73771f35")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC_Core_Quan_Ly_Trung_Tam_TA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC_Core_Quan_Ly_Trung_Tam_TA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
