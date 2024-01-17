43e40bbc973b77f531c9df269ac4bafc9189afee
mannhu78 <mannhu593@gmail.com>
2024-01-17 15:10:49 +0700

    giangvien

--- MVC_Core_Quan_Ly_Trung_Tam_TA/DataBase/GiangVienContext.cs
+++ MVC_Core_Quan_Ly_Trung_Tam_TA/DataBase/GiangVienContext.cs
@@ -14,7 +14,7 @@ namespace MVC_Core_Quan_Ly_Trung_Tam_TA.DataBase
             if (!optionbuilder.IsConfigured)
             {
                 // string sqlConnetionSanbox = $"Data Source={SystemEnv.ServerSanboxAddress}; database={SystemEnv.SanboxDatabaseName}; User Id={SystemEnv.UserNameSanbox}; Password={SystemEnv.PasswordSanbox}; TrustServerCertificate=True";
-                string sqlConnetionSanbox = $"Data Source=ADMIN-PC,1433; database=StudentManagement; User Id=hoangdm; Password=1234; TrustServerCertificate=True";
+                string sqlConnetionSanbox = $"Data Source=ADMIN-PC,1433; database=QL_TTTA; User Id=mannhu; Password=mannhu; TrustServerCertificate=True";
                 optionbuilder.UseSqlServer(sqlConnetionSanbox);
             }
         }
